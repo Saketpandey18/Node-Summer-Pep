d82eea001252e748e78fc9f688763d6f520f9db2
Saket Pandey <97404335+Saketpandey18@users.noreply.github.com>
2023-07-21 15:44:44 +0530

    added mongodb cloud

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ app.get("/:shortid", async (req, res) => {
   res.redirect(rec.full);
 });
 
-mongoose.connect("mongodb://0.0.0.0/urlShortner", {
+mongoose.connect("mongodb+srv://saket:<password>@cluster0.gii3i3r.mongodb.net/", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
